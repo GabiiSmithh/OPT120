10d9bf1c62fc358c2a96c88388feb5fde7ebc17d
GabiiSmithh <142529563+GabiiSmithh@users.noreply.github.com>
2024-04-01 21:04:41 -0300

    Add files via upload

--- frontend_devmovel/lib/cadastro_usuario.dart
+++ frontend_devmovel/lib/cadastro_usuario.dart
@@ -1,78 +1,168 @@
 import 'package:flutter/material.dart';
+import 'cadastro_usuario.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cadastro',
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        hintColor: Colors.purpleAccent,
+        fontFamily: 'Roboto',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.teal),
+          bodyText1: TextStyle(fontSize: 16.0, color: Colors.black87),
+          button: TextStyle(fontSize: 18.0, color: Colors.white),
+        ),
+      ),
+      home: CadastroUsuarioPage(),
+    );
+  }
+}
 
 class CadastroUsuarioPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _nome = '';
+  String _email = '';
+  String _senha = '';
+
+  void _cadastrar() {
+    if (_formKey.currentState!.validate()) {
+      // Aqui você faria o request para o backend com os dados do formulário
+      print('Nome: $_nome');
+      print('Email: $_email');
+      print('Senha: $_senha');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cadastro de Usuário'),
+        title: Text('Cadastro'),
       ),
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _buildTextFieldWithLabel("Nome", Icons.person),
-            SizedBox(height: 20),
-            _buildTextFieldWithLabel("Email", Icons.email),
-            SizedBox(height: 20),
-            _buildTextFieldWithLabel("Senha", Icons.lock, obscureText: true),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: () {
-                // Implementar a lógica de salvar
-              },
-              icon: Icon(Icons.save),
-              label: Text('Salvar', style: TextStyle(color: Colors.white)),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200], // Define a cor de fundo desejada
         ),
-      ),
-    );
-  }
-
-  Widget _buildTextFieldWithLabel(String labelText, IconData iconData,
-      {bool obscureText = false}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(iconData, size: 20),
-            SizedBox(width: 8),
-            Text(
-              labelText,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.8, // Define a largura dos inputs
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                        labelStyle: TextStyle(color: Colors.teal),
+                        prefixIcon: Icon(Icons.person, color: Colors.teal), // Ícone no início do campo de texto
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal.withOpacity(0.5)),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira seu nome';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _nome = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.8, // Define a largura dos inputs
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Colors.teal),
+                        prefixIcon: Icon(Icons.email, color: Colors.teal), // Ícone no início do campo de texto
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal.withOpacity(0.5)),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira seu email';
+                        }
+                        if (!value.contains('@')) {
+                          return 'Email inválido';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _email = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.8, // Define a largura dos inputs
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(color: Colors.teal),
+                        prefixIcon: Icon(Icons.lock, color: Colors.teal), // Ícone no início do campo de texto
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.teal.withOpacity(0.5)),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, insira sua senha';
+                        }
+                        if (value.length < 6) {
+                          return 'A senha deve conter pelo menos 6 caracteres';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _senha = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.5, // Define a largura do botão
+                    child: ElevatedButton(
+                      onPressed: _cadastrar,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.teal, // Altera a cor de fundo do botão
+                      ),
+                      child: Text(
+                        'Cadastrar',
+                        style: TextStyle(color: Colors.white), // Altera a cor do texto do botão
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-        SizedBox(height: 8),
-        Container(
-          constraints: BoxConstraints(maxWidth: 300), // Define o tamanho máximo
-          child: TextFormField(
-            obscureText: obscureText,
-            decoration: InputDecoration(
-              hintText: 'Digite seu $labelText',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              filled: true,
-              fillColor: Colors.purple[50],
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
